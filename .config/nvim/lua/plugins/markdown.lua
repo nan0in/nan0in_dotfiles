23d75c5fbb48506ea0cc9da3c26b6b8363f956b9
Nan <2436378140@qq.com>
2025-12-21 20:04:10 +0800

    create a single file for rendering and editoring markdown

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,106 @@
+return {
+  -- Markdown
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons", -- 强制添加这个依赖，这是最通用的图标库
+    },
+    opts = {
+      -- 显式开启渲染功能，防止被误关
+      render_modes = { "n", "c", "t" }, -- 在普通模式和命令模式下渲染
+      checkbox = {
+        enabled = true,
+      },
+      code = {
+        width = "block",
+        left_pad = 2,
+        right_pad = 4,
+      },
+      -- 标题渲染配置 start
+      heading = {
+        enabled = true,
+        render_modes = false,
+        atx = true,
+        setext = true,
+        sign = true,
+        icons = { "󰼏 ", "󰎨 " },
+        position = "overlay",
+        signs = { "󰫎 " },
+        width = "full",
+        left_margin = 0,
+        left_pad = 0,
+        right_pad = 0,
+        min_width = 0,
+        border = false,
+        border_virtual = false,
+        border_prefix = false,
+        above = "▄",
+        below = "▀",
+        backgrounds = {
+          "RenderMarkdownH1Bg",
+          "RenderMarkdownH2Bg",
+          "RenderMarkdownH3Bg",
+          "RenderMarkdownH4Bg",
+          "RenderMarkdownH5Bg",
+          "RenderMarkdownH6Bg",
+        },
+        foregrounds = {
+          "RenderMarkdownH1",
+          "RenderMarkdownH2",
+          "RenderMarkdownH3",
+          "RenderMarkdownH4",
+          "RenderMarkdownH5",
+          "RenderMarkdownH6",
+        },
+        custom = {},
+      },
+      -- 标题渲染配置 end
+    },
+    ft = { "markdown", "norg", "rmd", "org" },
+  },
+  {
+    "jakewvincent/mkdnflow.nvim",
+    ft = "markdown", -- 仅在 markdown 文件加载
+    config = function()
+      require("mkdnflow").setup({
+        mappings = {
+          MkdnEnter = { { "n", "v", "i" }, "<CR>" }, -- 回车键增强：列表续行、打开链接
+          MkdnTab = false, -- 禁用 tab 避免冲突，或者设为 true 用于缩进列表
+          MkdnFoldSection = false,
+        },
+        perspective = {
+          priority = "root",
+        },
+      })
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    config = function()
+      -- 可选：设置快捷键
+      vim.g.mkdp_theme = "dark" -- 暗色主题
+    end,
+  },
+  {
+    "3rd/image.nvim",
+    opts = {
+      backend = "kitty", -- 或者 "ueberzug"
+      tmux_show_only_in_active_window = true,
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = true,
+          download_remote_images = true, -- 甚至能渲染网络图片
+          only_render_image_at_cursor = true,  --只渲染光标下的图片 
+          only_render_image_at_cursor_mode = "popup",  --只渲染光标下的图片 
+        },
+      },
+    },
+  },
+}
