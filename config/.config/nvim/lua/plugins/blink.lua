f9e3649c858120d029782207fec81cd6bf4c8e26
Nan <2436378140@qq.com>
2026-06-21 15:16:13 +0800

    Add blink completion trigger config

---
+++ config/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<C-Space>"] = { "show" },
+      },
+      completion = {
+        menu = {
+          auto_show = true,
+        },
+        trigger = {
+          show_on_insert = true,
+        },
+      },
+    },
+  },
+}
